573aefe3af27f6a36b177ca79aa463b372cbe621
Brady Amundson <brady.amundson98@gmai.com>
2024-08-25 20:10:24 -0700

    fixed min group bug

--- src/pages/Classroom.js
+++ src/pages/Classroom.js
@@ -993,7 +993,7 @@ const Classroom = () => {
   };
 
   const decrementMinGroupSize = () => {
-    if (minGroupSize > 0) {
+    if (minGroupSize > 1) {
       setMinGroupSize((prevSize) => prevSize - 1);
     }
 
